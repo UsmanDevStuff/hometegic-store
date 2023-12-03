30bd3cdbbdcfc0358777da4716fa84f47304b916
Muhammad Usman <entpnr.usman@gmail.com>
2023-12-03 21:21:27 +0500

    styling /tc page

--- app/(routes)/tc/page.tsx
+++ app/(routes)/tc/page.tsx
@@ -4,95 +4,102 @@ export default function TC() {
   return (
     <section>
       <Container>
-        <div>
-          <h1>Terms and Conditions</h1>
-          <p>
-            Please read these Terms and Conditions carefully before using the
-            ShopTegic website operated by ShopTegic.
-          </p>
+        <div className="my-6 mx-4 md:mx-8 lg:mx-20">
+          <div>
+            <h1 className="text-2xl font-extrabold">Terms and Conditions</h1>
+            <p className="font-semibold">
+              Please read these Terms and Conditions carefully before using the
+              ShopTegic website operated by ShopTegic.
+            </p>
+            <br />
+            <p className="font-semibold">
+              By accessing or using the Website, you agree to be bound by these
+              Terms and Conditions. If you disagree with any part of these Terms
+              and Conditions, you may not access the Website.
+            </p>
+          </div>
           <br />
-          <p>
-            By accessing or using the Website, you agree to be bound by these
-            Terms and Conditions. If you disagree with any part of these Terms
-            and Conditions, you may not access the Website.
-          </p>
-        </div>
-        <br />
-        <div>
-          <p>
-            Products and Services: 1.1. ShopTegic offers various products and
-            services for sale on the Website. 1.2. The products and services are
-            subject to availability and may be amended, discontinued, or
-            modified at any time without prior notice.
-          </p>
-          <p>
-            Orders and Payment: 2.1. By placing an order on the Website, you
-            agree to provide accurate and complete information. 2.2. ShopTegic
-            reserves the right to refuse or cancel any order for any reason at
-            any time. 2.3. Payment for orders must be made in full at the time
-            of purchase. 2.4. ShopTegic uses secure payment gateways to process
-            transactions and does not store any payment information.
-          </p>
-          <p>
-            Shipping and Delivery: 3.1. ShopTegic will make reasonable efforts
-            to ensure timely delivery of products. 3.2. Shipping and delivery
-            times may vary based on location and availability. 3.3. Any customs
-            duties, taxes, or fees incurred during shipping are the
-            responsibility of the customer.
-          </p>
-          <p>
-            Returns and Refunds: 4.1. ShopTegic offers a 30-day return policy
-            for eligible products. 4.2. To initiate a return, please contact our
-            customer support team. 4.3. Refunds will be issued within a
-            reasonable timeframe after the returned item is received and
-            inspected. 4.4. Shipping fees are non-refundable, and return
-            shipping costs are the responsibility of the customer unless the
-            return is due to a defective or incorrect product.
-          </p>
-          <p>
-            Intellectual Property: 5.1. All content on the Website, including
-            but not limited to images, logos, text, and graphics, are the
-            intellectual property of ShopTegic and protected by applicable
-            copyright and trademark laws. 5.2. You may not use, reproduce,
-            distribute, or modify any content from the Website without the prior
-            written consent of ShopTegic.
-          </p>
-          <p>
-            Limitation of Liability: 6.1. ShopTegic shall not be liable for any
-            direct, indirect, incidental, consequential, or punitive damages
-            arising out of or related to the use of the Website or the products
-            and services offered. 6.2. ShopTegic does not guarantee the
-            accuracy, completeness, or reliability of any information on the
-            Website.
-          </p>
-          <p>
-            Indemnification: 7.1. You agree to indemnify and hold harmless
-            ShopTegic, its affiliates, employees, and agents from any claims,
-            damages, losses, liabilities, and expenses arising out of or related
-            to your use of the Website or violation of these Terms and
-            Conditions.
-          </p>
-          <p>
-            Governing Law: 8.1. These Terms and Conditions shall be governed by
-            and construed in accordance with the laws of United States. 8.2. Any
-            legal action or proceeding arising out of or related to these Terms
-            and Conditions shall be brought exclusively in the courts of United
-            States.
-          </p>
-          <p>
-            Changes to Terms and Conditions: 9.1. ShopTegic reserves the right
-            to modify or replace these Terms and Conditions at any time without
-            prior notice. 9.2. Continued use of the Website after any such
-            changes shall constitute your consent to the modified Terms and
-            Conditions.
-          </p>
-        </div>
-        <br />
-        <div>
-          <p>
-            By using the ShopTegic Website, you acknowledge that you have read,
-            understood, and agree to be bound by these Terms and Conditions.
-          </p>
+          <div>
+            <p>
+              <span className="font-bold">Products and Services</span>: 1.1.
+              ShopTegic offers various products and services for sale on the
+              Website. 1.2. The products and services are subject to
+              availability and may be amended, discontinued, or modified at any
+              time without prior notice.
+            </p>
+            <p>
+              <span className="font-bold">Orders and Payment</span>: 2.1. By
+              placing an order on the Website, you agree to provide accurate and
+              complete information. 2.2. ShopTegic reserves the right to refuse
+              or cancel any order for any reason at any time. 2.3. Payment for
+              orders must be made in full at the time of purchase. 2.4.
+              ShopTegic uses secure payment gateways to process transactions and
+              does not store any payment information.
+            </p>
+            <p>
+              <span className="font-bold">Shipping and Delivery</span>: 3.1.
+              ShopTegic will make reasonable efforts to ensure timely delivery
+              of products. 3.2. Shipping and delivery times may vary based on
+              location and availability. 3.3. Any customs duties, taxes, or fees
+              incurred during shipping are the responsibility of the customer.
+            </p>
+            <p>
+              <span className="font-bold">Returns and Refunds</span>: 4.1.
+              ShopTegic offers a 30-day return policy for eligible products.
+              4.2. To initiate a return, please contact our customer support
+              team. 4.3. Refunds will be issued within a reasonable timeframe
+              after the returned item is received and inspected. 4.4. Shipping
+              fees are non-refundable, and return shipping costs are the
+              responsibility of the customer unless the return is due to a
+              defective or incorrect product.
+            </p>
+            <p>
+              <span className="font-bold">Intellectual Property</span>: 5.1. All
+              content on the Website, including but not limited to images,
+              logos, text, and graphics, are the intellectual property of
+              ShopTegic and protected by applicable copyright and trademark
+              laws. 5.2. You may not use, reproduce, distribute, or modify any
+              content from the Website without the prior written consent of
+              ShopTegic.
+            </p>
+            <p>
+              <span className="font-bold">Limitation of Liability</span>: 6.1.
+              ShopTegic shall not be liable for any direct, indirect,
+              incidental, consequential, or punitive damages arising out of or
+              related to the use of the Website or the products and services
+              offered. 6.2. ShopTegic does not guarantee the accuracy,
+              completeness, or reliability of any information on the Website.
+            </p>
+            <p>
+              <span className="font-bold">Indemnification</span>: 7.1. You agree
+              to indemnify and hold harmless ShopTegic, its affiliates,
+              employees, and agents from any claims, damages, losses,
+              liabilities, and expenses arising out of or related to your use of
+              the Website or violation of these Terms and Conditions.
+            </p>
+            <p>
+              <span className="font-bold">Governing Law</span>: 8.1. These Terms
+              and Conditions shall be governed by and construed in accordance
+              with the laws of United States. 8.2. Any legal action or
+              proceeding arising out of or related to these Terms and Conditions
+              shall be brought exclusively in the courts of United States.
+            </p>
+            <p>
+              <span className="font-bold">Changes to Terms and Conditions</span>
+              : 9.1. ShopTegic reserves the right to modify or replace these
+              Terms and Conditions at any time without prior notice. 9.2.
+              Continued use of the Website after any such changes shall
+              constitute your consent to the modified Terms and Conditions.
+            </p>
+          </div>
+          <br />
+          <div>
+            <p className="font-semibold italic">
+              By using the ShopTegic Website, you acknowledge that you have
+              read, understood, and agree to be bound by these Terms and
+              Conditions.
+            </p>
+          </div>
         </div>
       </Container>
     </section>
